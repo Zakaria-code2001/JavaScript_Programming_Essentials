8c43e562810821bc7561e5146c5a45db8b00baa3
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-06-29 04:00:32 -0400

    socpes

---
+++ basic_js/scope_lab.js
@@ -0,0 +1,38 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+{
+// Block scope
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
+
+// console.log(blockVar);
+// console.log(blockLet);
+
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+    }
+    show();
+    
+    console.log(functionVar); // Throws ReferenceError
+    console.log(functionLet); // Throws ReferenceError
+    console.log(functionConst); // Throws ReferenceError
+
+{
+    const me = 'Zakaria Mohammadi'
+    let pc = 'Mac Mini'
+    var headphones = 'Soundcore'
+    const me = 'Zack Moha'
+    let pc = 'Hp pavillion'
+    var headphones = 'Airpods'
+}
+
+
+const me = 'Zakaria Mohammadi'
+let pc = 'Mac Mini'
+var headphones = 'Soundcore'
